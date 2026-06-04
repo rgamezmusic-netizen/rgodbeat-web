7880634790c25fd590747d65c8f321220d44d771
RGodbeat <rgamezmusic@gmail.com>
2026-06-04 17:35:57 -0500

    Update server

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
     storage: storage,
-    limits: { fileSize: 100 * 1024 * 1024 } // Limit: 100MB
+    limits: { fileSize: 500 * 1024 * 1024 } // Limit: 500MB (increased from 100MB)
 });
 
 const app = express();
